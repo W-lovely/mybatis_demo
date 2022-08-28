884954f12285ff33d35fcb2032e85029738c8869
saber <1751102935@qq.com>
2022-08-28 13:47:59 +0800

    devfz1

--- src/main/java/com/wzp/git/GitTest.java
+++ src/main/java/com/wzp/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello,git!4");
         System.out.println("hello,git!4");
         System.out.println("hello,git!5");
+        System.out.println("hello,git!devfz");
     }
 }
