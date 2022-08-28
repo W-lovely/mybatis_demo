3ce3a6d20a684e4b5f04e9d46e91a9e8a6236969
saber <1751102935@qq.com>
2022-08-28 13:53:35 +0800

    devzfz1

--- src/main/java/com/wzp/git/GitTest.java
+++ src/main/java/com/wzp/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hello,git!4");
         System.out.println("hello,git!5");
         System.out.println("hello,git!devfz");
+        System.out.println("hello,git!devzfz");
+
     }
 }
